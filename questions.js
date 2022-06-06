b44d7ed45dff209fb34e7f220e95f89742a11e8f
Navneet Kaur <nbirdi@yahoo.com>
2022-06-06 22:12:51 +0800

    initial commits

---
+++ questions.js
@@ -0,0 +1,102 @@
+const questions = [
+    {
+      title: "Inside which element do we put JavaScript",
+      choices: [
+        {
+          title: "<scripting>",
+          isAnswer: false,
+        },
+        {
+          title: "<javascript>",
+          isAnswer: false,
+        },
+        {
+          title: "<script>",
+          isAnswer: true,
+        },
+      ],
+    },
+    {
+      title: "How do you call a function named myFunction()? ",
+      choices: [ 
+        {
+          title: "myFunction()",
+          isAnswer: true,
+        },
+        {
+          title: "call myFunction()",
+          isAnswer: false,
+        },
+        {
+          title: "call function myFunction()",
+          isAnswer: false,
+        },
+      ],
+    },
+    {
+      title: "How can you add a comment in JavaScript?",
+      choices: [
+        {
+          title: "/*This is a comment*/",
+          isAnswer: false,
+        },
+        {
+          title: "<!This is a comment-->",
+          isAnswer: false,
+        },
+        {
+          title: "//This is a comment",
+          isAnswer: true,
+        },
+      ],
+    },
+    {
+      title: "Which operator is used to assign a value to a variable?",
+      choices: [
+        {
+          title: "-",
+          isAnswer: false,
+        },
+        {
+          title: "*",
+          isAnswer: false,
+        },
+        {
+          title: "=",
+          isAnswer: true,
+        },
+      ],
+    },
+    {
+        title: "What will the following code return: Boolean(10>9)",
+        choices: [
+          {
+            title: "false",
+            isAnswer: false,
+          },
+          {
+            title: "NaN",
+            isAnswer: false,
+          },
+          {
+            title: "true",
+            isAnswer: true,
+          },
+        ],
+      },
+      {
+        title: "Is JavaScript case sensitive?",
+        choices: [
+          
+          {
+            title: "no",
+            isAnswer: false,
+          },
+          {
+            title: "yes",
+            isAnswer: true,
+          },
+        ],
+      },
+  ];
+  
