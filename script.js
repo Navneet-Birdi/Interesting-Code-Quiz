acc86fe8847ecae7f63cfba10dc78954de7ee199
Navneet Kaur <nbirdi@yahoo.com>
2022-06-08 12:39:32 +0800

    scores fixed

--- script.js
+++ script.js
@@ -123,6 +123,7 @@ function endGame(){
   // stop the timer
   clearInterval(timerId);
   //console.log('timer stopped');
+  document.querySelector('#result-span').textContent = timeRemaining
 }
 
 function startTimer(){
